5d6a0cd67699e7e0b9e628fab504cddcdb1d25a2
Lucas Lai <llai@xogrp.com>
2019-05-07 15:29:08 +0800

    redux state

--- App.js
+++ App.js
@@ -6,50 +6,95 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStore } from 'redux';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// Define action types
+const types = {
+  INCREMENT: 'INCREMENT',
+}
 
-class HomeScreen extends React.Component {
+// Define a reducer
+const reducer = (state, action) => {
+  if (action.type === types.INCREMENT) {
+    return {count: state.count + 1}
+  }
+  return state
+}
+
+// Define the initial state of our store
+const initialState = {count: 0}
+
+// Create a store, passing our reducer function and our initial state
+const store = createStore(reducer, initialState)
+
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+
+class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 1
+    };
+  }
   static navigationOptions = {
-    title: 'Home',
+    title: "Home"
   };
+
+  componentDidMount() {
+    // setInterval(() => (
+    //   this.setState(previousState => (
+    //     { num: previousState.num++ }
+    //   ))
+    // ), 1000);
+  }
+  add = () => {
+    this.setState({num: this.state.num++}, () => {
+      console.warn(`abcd${this.state.num}`);
+    });
+  }
   render() {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigate('Profile', {name: 'Jane'})}
-      />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home screen{store.getState().count}</Text>
+        <Text>{this.state.num}</Text>
+
+        <Button
+          title="Go to Jane's profile"
+          onPress={() => navigate("Profile", { name: "Jane" })}
+        />
+        <Button title="Add" onPress={this.add} />
+      </View>
     );
   }
 }
 
-class ProfileScreen extends React.Component {
+class ProfileScreen extends Component {
   static navigationOptions = {
-    title: 'Profile',
+    title: "Profile"
   };
   render() {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
-      <Button
-        title="Go to Home"
-        onPress={() => navigate('Home', {name: 'Home'})}
-      />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Profile screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => navigate("Home", { name: "Home" })}
+        />
+      </View>
     );
   }
 }
 
 const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Profile: {screen: ProfileScreen},
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen }
 });
 
 const App = createAppContainer(MainNavigator);
@@ -59,18 +104,18 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
